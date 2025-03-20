8c7c454b5de94b9a4e70bb937b7d8b7e95cc49b7
ernazarov51 <ravshantursunboyev55@gmail.com>
2025-03-20 12:07:06 +0500

    add requirements

--- apps/admin.py
+++ apps/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from apps.models import User
+from apps.models import User, Science, UserScience
 
 admin.site.register(User)
+admin.site.register(Science)
+admin.site.register(UserScience)
